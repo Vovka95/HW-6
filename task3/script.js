dbe099dcbbf6478132911c60fa693fd96c0e5fe3
Vovka95 <volodimir.protskiy@gmail.com>
2018-09-07 16:11:30 +0300

    update class User

--- task3/script.js
+++ task3/script.js
@@ -2,81 +2,53 @@ const form = document.forms.namedItem("user");
 const users = [];
 const tBody = document.createElement('tbody');
 let count = 0;
+const table = document.querySelector('table');
 
-
+table.appendChild(tBody);
 
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 
-
-	
 	let user = new User(this.name, this.sex, this.date, this.address, this.phone, this.email);
 
 	users.push(user);
-	
 
-	user.render(count);
-	let tr = user.render(count);
-
+	const tr = user.render(count);
 	user.createEventClick(tr);
-	addU(tr);
-
+	addUser(tr);
 
 	count++;
-
 })
 
-
-
-
 class SuperUser {
-	constructor(name, sex, date, address, phone, email) {
+	constructor() {
 		this.name = document.getElementById("name").value;
 		this.sex = document.getElementById("sex").value;
 		this.date = document.getElementById("birthdate").value;
 		this.address = document.getElementById("address").value;
 		this.phone = document.getElementById("phone").value;
 		this.email = document.getElementById("email").value;
-	}
+	}	
+}
+
+function addUser(tr) {
+	tBody.appendChild(tr);
+}
+
+class User extends SuperUser {
 
 	render(count) {
 	
 		const tr = document.createElement('tr');
-		const table = document.querySelector('table');
 		tr.setAttribute('user-index',count);
-		
-
-		
 
 		for (let key in this) {
 			
 			const td = document.createElement('td');
-			
-	      td.textContent = this[key];
+			td.textContent = this[key];
 	      tr.appendChild(td);  
 		}
-
-		table.appendChild(tBody);
-		
-
 		return tr;
-	}
-
-	
-}
-
-
-
-
-function addU(tr) {
-	tBody.appendChild(tr);
-}
-
-
-
-class User extends SuperUser {
-	constructor(name, sex, date, address, phone, email){
-		super(name, sex, date, address, phone, email)
 	}
 	
 	changeDataVisibility(tag) {
@@ -98,10 +70,8 @@ class User extends SuperUser {
 	   const index = tag.getAttribute("user-index");
 
 	   users[index].changeDataVisibility(tag);
-	        
-	        	
-   	});
-        
+     	
+   	});    
 	}
 }
 
